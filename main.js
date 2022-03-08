2fd94dc1fc85cd047ed75d942e65a851cf0309f0
Insane-Vaas <hrithikr200@gmail.com>
2022-03-08 14:19:59 +0530

    Detecting youtube videos and stoping

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 const re1 = new RegExp(/\b(?:coding|programming|coding|java|dart|flutter|c|js|nodejs)\b/gi)
-function onRemoved() {
-    console.log(`Removed`);
-  }
-  
-function onError(error) {
-    console.log(`Error: ${error}`);
-  
-}
 
-async function getCurrentTab() {
-    let queryOptions = { active: true, currentWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
-}
+var arr = new Array;
 
-function mainFunction(){
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    
+    if (request.message === 'URLCHANGED!') {
+        
+        arr.push(request.url);
+        if(arr.length%4 === 0)mainFunction();
+    }
+});
+
+async function mainFunction(){
 
     var item = item = document.getElementsByClassName('style-scope ytd-video-primary-info-renderer');
     var dis = dis = document.getElementsByClassName('content style-scope ytd-video-secondary-info-renderer');
@@ -25,21 +23,16 @@ function mainFunction(){
     const matche1 = str1.match(re1);
     const matche2 = str2.match(re1);
 
-    console.log(getCurrentTab());
-
     if((matche1 != null && matche1.length > 0) || (matche2 != null && matche2.length > 0)){
 
         console.log("Learning Detected!!");
 
     }
     else{
-        
+        closeWindow();
         console.log("Masti Nhi");
         play_pause[0].click();
-        var removing = chrome.tabs.remove();
-        removing.then(onRemoved, onError);
-
-
+        
     }
 
 }
